e8ed3b6465eb34b37a5d259acca9ebd424d49fe0
a-ragab-atlasone <ahmed@atlasone.io>
2022-04-24 21:23:07 +0200

    remove logs folder and add validations to auth apis

--- src/api/routes/auth/index.ts
+++ src/api/routes/auth/index.ts
@@ -1,14 +1,18 @@
 import express, { Router as ExpressRouter } from "express";
 
 import * as authController from '../../controllers/auth';
+import { celebrate } from "celebrate";
+import { usersSchema } from "../../../middlewares/joiValidations";
 
 const Router: ExpressRouter = express.Router();
 
 Router.route("/register").post(
+  celebrate(usersSchema.AuthUsersSchema),
   authController.register
 );
 
 Router.route("/login").post(
+  celebrate(usersSchema.AuthUsersSchema),
   authController.login
 );
 
